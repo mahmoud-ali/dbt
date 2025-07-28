34586be321ce16ae50ebbf665a4090fd0a814a93
Mahmoud Ali <mahmoud.webapps@gmail.com>
2025-07-28 17:47:16 +0200

    change kpi to days

--- mineralsgate/models/sswg/sswg_traditional_export_gold_finished.sql
+++ mineralsgate/models/sswg/sswg_traditional_export_gold_finished.sql
@@ -1,5 +1,4 @@
 SELECT
-  "public"."sswg_customforceairportdata"."comment" AS "customforce_comment", 
   "sswg_basicformexport"."export_id" AS "export_id",
   "sswg_basicformexport"."date" AS "export_date",
   "sswg_basicformexport"."sn_no" AS "export_sn_no",
@@ -7,10 +6,10 @@ SELECT
   "sswg_basicformexport"."next_state" AS "export_next_state",
   "sswg_basicformexport"."company_name" AS "company_name",
   "sswg_basicformexport"."link" AS "link",
-  "sswg_basicformexport"."created_at" AS "started_at",
-  "public"."sswg_customforceairportdata"."created_at" AS "ended_at",
-  FLOOR(EXTRACT(EPOCH FROM ("public"."sswg_customforceairportdata"."created_at" - "sswg_basicformexport"."created_at"))/3600) as "kpi_hours"
+  "sswg_basicformexport"."date" AS "started_at",
+  "public"."sswg_unifiedteamdata"."flight_datetime" AS "ended_at",
+  FLOOR(EXTRACT(EPOCH FROM ("public"."sswg_unifiedteamdata"."flight_datetime" - "sswg_basicformexport"."date"))/86400)+1 as "kpi_days"
 FROM
-  "public"."sswg_customforceairportdata"
+  "public"."sswg_unifiedteamdata"
 INNER JOIN 
-    {{ ref('sswg_traditional_export_gold')}} AS "sswg_basicformexport" ON "public"."sswg_customforceairportdata"."basic_form_export_id" = "sswg_basicformexport"."export_id"
+    {{ ref('sswg_traditional_export_gold')}} AS "sswg_basicformexport" ON "public"."sswg_unifiedteamdata"."basic_form_export_id" = "sswg_basicformexport"."export_id"
