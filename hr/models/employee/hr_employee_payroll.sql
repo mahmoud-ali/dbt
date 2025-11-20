85c1d92c492f61c697d28b2b921fb764524ed926
Mahmoud Ali <mahmoud.webapps@gmail.com>
2025-11-20 09:45:24 +0200

    add employee_payroll view

---
+++ hr/models/employee/hr_employee_payroll.sql
@@ -0,0 +1,10 @@
+SELECT
+  "emp".*,
+  make_date("master"."year", "master"."month", 27) AS "date",
+  "payroll"."total_salary",
+  "payroll"."net_salary"
+
+FROM
+  "public"."hr_payrollsummary" as "payroll"
+LEFT JOIN "public"."hr_payrollmaster" as "master" ON "master"."id" = "payroll"."payroll_master_id" 
+LEFT JOIN {{ ref('hr_employee')}} as "emp" ON "emp"."id" = "payroll"."employee_id" 
