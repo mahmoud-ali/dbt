03402d691a0e4af27572ca326c6a9897fcd1eefe
Mahmoud Ali <mahmoud.webapps@gmail.com>
2025-12-01 13:13:04 +0200

    remove unused join for forwarded_to in maktab_tanfizi_gm_movement.sql

--- mineralsgate/models/maktab_tanfizi/maktab_tanfizi_gm_movement.sql
+++ mineralsgate/models/maktab_tanfizi/maktab_tanfizi_gm_movement.sql
@@ -13,14 +13,12 @@ SELECT
   "public"."khatabat_harkatkhatabat"."note" AS "haraka_note",
   "public"."khatabat_maktabtanfizi"."name" AS "maktab_tanfizi_name",
   "khatabat_maktabtanfizijiha_source"."name" AS "haraka_source_jiha",
-  "khatabat_maktabtanfizijiha_forwarded_to"."name" AS "haraka_forwarded_to_jiha",
   CONCAT('https://mineralsgate.com/app/managers/khatabat/khatabat/',"public"."khatabat_khatabat"."letter_number",'/change/') as "link"
 FROM
   "public"."khatabat_khatabat"
 INNER JOIN "public"."khatabat_harkatkhatabat" ON "public"."khatabat_khatabat"."letter_number" = "public"."khatabat_harkatkhatabat"."letter_id"
 LEFT JOIN "public"."khatabat_maktabtanfizi" ON "public"."khatabat_khatabat"."maktab_tanfizi_id" = "public"."khatabat_maktabtanfizi"."id"
 LEFT JOIN "public"."khatabat_maktabtanfizijiha" AS "khatabat_maktabtanfizijiha_source" ON "public"."khatabat_harkatkhatabat"."source_entity_id" = "khatabat_maktabtanfizijiha_source"."id"
-LEFT JOIN "public"."khatabat_maktabtanfizijiha" AS "khatabat_maktabtanfizijiha_forwarded_to" ON "public"."khatabat_harkatkhatabat"."forwarded_to_id" = "khatabat_maktabtanfizijiha_forwarded_to"."id"
 LEFT JOIN {{ref('maktab_tanfizi_procedure')}} AS "maktab_tanfizi_procedure" ON "public"."khatabat_harkatkhatabat"."procedure" = "maktab_tanfizi_procedure"."id"
 LEFT JOIN {{ref('maktab_tanfizi_movement_type')}} AS "maktab_tanfizi_movement_type" ON "public"."khatabat_harkatkhatabat"."movement_type" = "maktab_tanfizi_movement_type"."id"
 LEFT JOIN {{ref('maktab_tanfizi_followup')}} AS "maktab_tanfizi_followup" ON "public"."khatabat_harkatkhatabat"."followup_result" = "maktab_tanfizi_followup"."id"
