a41103a7cffec716bad4a9b7d22efdd0a5e6feca
Mahmoud Ali <mahmoud.webapps@gmail.com>
2025-12-11 14:05:20 +0200

    add motab3a view

---
+++ mineralsgate/models/maktab_tanfizi/maktab_tanfizi_gm_motab3a.sql
@@ -0,0 +1,16 @@
+SELECT
+  "public"."khatabat_khatabat"."letter_number" AS "letter_number",
+  "public"."khatabat_khatabat"."subject" AS "subject",
+  "public"."khatabat_khatabat"."updated_at" AS "updated_at",
+  "public"."khatabat_khatabat"."created_at" AS "created_at",
+  "public"."khatabat_motab3atkhatabat"."id" AS "motab3a_id",
+  "public"."khatabat_maktabtanfizi"."name" AS "maktab_tanfizi_name",
+  "public"."khatabat_motab3atkhatabat"."date" AS "date",
+  "public"."khatabat_motab3atkhatabat"."action" AS "action",
+  "public"."khatabat_motab3atkhatabat"."done" AS "done",
+  CONCAT('https://mineralsgate.com/app/managers/khatabat/khatabat/',"public"."khatabat_khatabat"."letter_number",'/change/') as "link"
+
+FROM  "public"."khatabat_khatabat"
+INNER JOIN "public"."khatabat_motab3atkhatabat" ON "public"."khatabat_khatabat"."letter_number" = "public"."khatabat_motab3atkhatabat"."letter_id"
+LEFT JOIN "public"."khatabat_maktabtanfizi" ON "public"."khatabat_khatabat"."maktab_tanfizi_id" = "public"."khatabat_maktabtanfizi"."id"
+WHERE LOWER("public"."khatabat_maktabtanfizi"."code") = 'gm' 
