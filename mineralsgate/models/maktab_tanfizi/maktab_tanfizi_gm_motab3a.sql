62537fdfbd38c989d43a9eb9da34c34662777fce
Mahmoud Ali <mahmoud.webapps@gmail.com>
2025-12-11 14:36:35 +0200

    translate true,false

--- mineralsgate/models/maktab_tanfizi/maktab_tanfizi_gm_motab3a.sql
+++ mineralsgate/models/maktab_tanfizi/maktab_tanfizi_gm_motab3a.sql
@@ -7,10 +7,11 @@ SELECT
   "public"."khatabat_maktabtanfizi"."name" AS "maktab_tanfizi_name",
   "public"."khatabat_motab3atkhatabat"."date" AS "date",
   "public"."khatabat_motab3atkhatabat"."action" AS "action",
-  "public"."khatabat_motab3atkhatabat"."done" AS "done",
+  "completion_state"."name" AS "done",
   CONCAT('https://mineralsgate.com/app/managers/khatabat/khatabat/',"public"."khatabat_khatabat"."letter_number",'/change/') as "link"
 
 FROM  "public"."khatabat_khatabat"
 INNER JOIN "public"."khatabat_motab3atkhatabat" ON "public"."khatabat_khatabat"."letter_number" = "public"."khatabat_motab3atkhatabat"."letter_id"
 LEFT JOIN "public"."khatabat_maktabtanfizi" ON "public"."khatabat_khatabat"."maktab_tanfizi_id" = "public"."khatabat_maktabtanfizi"."id"
+LEFT JOIN {{ref('completion_state')}} AS "completion_state" ON "completion_state"."id" = "public"."khatabat_motab3atkhatabat"."done"
 WHERE LOWER("public"."khatabat_maktabtanfizi"."code") = 'gm' 
