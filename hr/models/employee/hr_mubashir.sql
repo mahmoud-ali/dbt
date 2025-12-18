bf9f2dba9c004302c1be620bb227c70d05764adf
Mahmoud Ali <mahmoud.webapps@gmail.com>
2025-12-18 14:25:27 +0200

    mubashir list

---
+++ hr/models/employee/hr_mubashir.sql
@@ -0,0 +1,8 @@
+SELECT
+  "public"."hr_employee_survey_employee_data_emergency"."id" AS "id",
+  "public"."hr_employee_survey_employee_data_emergency"."name" AS "name",
+  "public"."hr_employee_survey_employee_data_emergency"."email" AS "email",
+  "public"."hr_employee_survey_employee_data_emergency"."direct_manager_email" AS "direct_manager_email",
+  "public"."hr_employee_survey_employee_data_emergency"."job_title" AS "job_title"
+FROM
+  "public"."hr_employee_survey_employee_data_emergency"
