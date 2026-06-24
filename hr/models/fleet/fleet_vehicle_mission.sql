d428fcb353b4d11dcda577eb3ab5147f5c72a7ed
alamin salah <alaminsalah9@gmail.com>
2026-06-24 11:08:22 +0200

    fix - query definition problem

--- hr/models/fleet/fleet_vehicle_mission.sql
+++ hr/models/fleet/fleet_vehicle_mission.sql
@@ -1,8 +1,10 @@
-
 SELECT 
-"public"."fleet_mission".*,
-"public"."fleet_driver"."name" AS "driver_name",
-"public"."fleet_vehicle"."fuel_type_id" AS "vehicle_fuel_type"
+  fleet_mission.*,
+  fleet_missionvehicle.id AS mission_vehicle_id,
+  COALESCE(fleet_vehicledriver.driver_id, fleet_mission.driver_id) AS assigned_driver_id,
+  COALESCE(fleet_vehicledriver.vehicle_id, fleet_mission.vehicle_id) AS assigned_vehicle_id,
+  fleet_driver.name AS driver_name
 FROM "public"."fleet_mission" AS fleet_mission
-left join "public"."fleet_driver"  AS fleet_driver ON fleet_driver.id = fleet_mission.driver_id
-left join "public"."fleet_vehicle" AS fleet_vehicle ON fleet_vehicle.id = fleet_mission.vehicle_id
+LEFT JOIN "public"."fleet_missionvehicle" AS fleet_missionvehicle ON fleet_mission.id = fleet_missionvehicle.mission_id
+LEFT JOIN "public"."fleet_vehicledriver" AS fleet_vehicledriver ON fleet_missionvehicle.assignment_id = fleet_vehicledriver.id
+LEFT JOIN "public"."fleet_driver"  AS fleet_driver ON fleet_driver.id = COALESCE(fleet_vehicledriver.driver_id, fleet_mission.driver_id)
