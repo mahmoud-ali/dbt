1547c9d1da100de447ad3f760c19c2a36bd3369a
Mahmoud Ali <mahmoud.webapps@gmail.com>
2026-07-13 16:05:49 +0200

    add - include created_by and updated_by usernames in gold travel traditional buy and melt SQL export queries

--- mineralsgate/models/export/gold_travel_traditional_buy.sql
+++ mineralsgate/models/export/gold_travel_traditional_buy.sql
@@ -14,11 +14,20 @@ SELECT
         WHEN 1 THEN 'دائري'
         WHEN 2 THEN 'مستطيل'
         WHEN 3 THEN 'أخرى'
-    END                                       AS alloy_shape
+    END                                       AS alloy_shape,
+    -- Created by / Updated by
+    cb.username AS created_by_username,
+    ub.username AS updated_by_username,
+
+    -- Attachement
+    CONCAT('https://mineralsgate.com/app/managers/gold_travel_traditional/appmovegoldtraditional/',s.id,'/change/') as "link",
+
 FROM gold_travel_traditional_sale s
 LEFT JOIN gold_travel_lkpowner oe ON s.buyer_exporter_id = oe.id
 LEFT JOIN gold_travel_traditional_lkpsaig sa ON s.buyer_saig_id = sa.id
 JOIN company_profile_lkpstate st ON s.source_state_id = st.id
 LEFT JOIN gold_travel_traditional_appmovegoldtraditional r ON r.sale_id = s.id
 LEFT JOIN gold_travel_traditional_appmovegoldtraditionaldetail d ON d.master_id = r.id
+LEFT JOIN accounts_customuser cb ON cb.id = s.created_by_id
+LEFT JOIN accounts_customuser ub ON ub.id = s.updated_by_id
 ORDER BY r.code, d.id
--- mineralsgate/models/export/gold_travel_traditional_melt.sql
+++ mineralsgate/models/export/gold_travel_traditional_melt.sql
@@ -11,9 +11,18 @@ SELECT
         WHEN 1 THEN 'دائري'
         WHEN 2 THEN 'مستطيل'
         WHEN 3 THEN 'أخرى'
-    END                                          AS alloy_shape
+    END                                          AS alloy_shape,
+    -- Created by / Updated by
+    cb.username AS created_by_username,
+    ub.username AS updated_by_username,
+
+    -- Attachement
+    CONCAT('https://mineralsgate.com/app/managers/gold_travel_traditional/appmovegoldtraditional/',s.id,'/change/') as "link",
+
 FROM gold_travel_traditional_meltbatch mb
 JOIN company_profile_lkpstate st ON mb.source_state_id = st.id
 LEFT JOIN gold_travel_traditional_appmovegoldtraditional r ON r.melt_batch_id = mb.id
 LEFT JOIN gold_travel_traditional_appmovegoldtraditionaldetail d ON d.master_id = r.id
+LEFT JOIN accounts_customuser cb ON cb.id = s.created_by_id
+LEFT JOIN accounts_customuser ub ON ub.id = s.updated_by_id
 ORDER BY r.code, d.id
